9e8d06fe918fa243b6f053bf6fd848dc0352955d
Shubham6822 <sharmashubham6822@gmail.com>
2025-03-21 12:29:10 +0530

    fix scroll issue

--- src/app/(dash)/dashboard/page.tsx
+++ src/app/(dash)/dashboard/page.tsx
@@ -6,61 +6,59 @@ import ShareForm from "../../../components/share-form";
 
 export default function DashboardPage() {
   return (
-    <div className="h-screen">
-      <div className="space-y-4">
-        <h1 className="text-3xl mb-6  text-left flex items-center gap-3 justify-between  ">
-          <div className=" items-center gap-1 text-2xl font-bold text-gray-900 dark:text-white">
-            <h1>Dashboard</h1>
-            <p className="text-gray-500 dark:text-gray-400 text-sm font-normal">
-              Your workspace for managing testimonials
-            </p>
-          </div>
-        </h1>
-        <div className="grid grid-cols-1 lg:grid-cols-3 gap-6">
-          <div className="bg-white dark:bg-[#0F0F12] rounded-xl p-6 flex flex-col border border-gray-200 dark:border-[#1F1F23]">
-            <h2 className="text-lg font-bold text-gray-900 dark:text-white mb-4 text-left flex items-center gap-2 ">
-              <MessageSquare className="w-3.5 h-3.5 text-zinc-900 dark:text-zinc-50" />
-              Testimonials
-            </h2>
-            <div className="flex-1">
-              <TestimonialStats type="total" />
-            </div>
-          </div>
-          <div className="bg-white dark:bg-[#0F0F12] rounded-xl p-6 flex flex-col border border-gray-200 dark:border-[#1F1F23] ">
-            <h2 className="text-lg font-bold text-gray-900 dark:text-white mb-4 text-left flex items-center gap-2">
-              <Share2 className="w-3.5 h-3.5 text-zinc-900 dark:text-zinc-50" />
-              Form Submissions
-            </h2>
-            <div className="flex-1">
-              <TestimonialStats type="submissions" />
-            </div>
-          </div>
-          <div className="bg-white dark:bg-[#0F0F12] rounded-xl p-6 flex flex-col border border-gray-200 dark:border-[#1F1F23]">
-            <h2 className="text-lg font-bold text-gray-900 dark:text-white mb-4 text-left flex items-center gap-2">
-              <Users className="w-3.5 h-3.5 text-zinc-900 dark:text-zinc-50" />
-              Team Members
-            </h2>
-            <div className="flex-1">
-              <TestimonialStats type="team" />
-            </div>
+    <div className="space-y-4">
+      <h1 className="text-3xl mb-6  text-left flex items-center gap-3 justify-between  ">
+        <div className=" items-center gap-1 text-2xl font-bold text-gray-900 dark:text-white">
+          <h1>Dashboard</h1>
+          <p className="text-gray-500 dark:text-gray-400 text-sm font-normal">
+            Your workspace for managing testimonials
+          </p>
+        </div>
+      </h1>
+      <div className="grid grid-cols-1 lg:grid-cols-3 gap-6">
+        <div className="bg-white dark:bg-[#0F0F12] rounded-xl p-6 flex flex-col border border-gray-200 dark:border-[#1F1F23]">
+          <h2 className="text-lg font-bold text-gray-900 dark:text-white mb-4 text-left flex items-center gap-2 ">
+            <MessageSquare className="w-3.5 h-3.5 text-zinc-900 dark:text-zinc-50" />
+            Testimonials
+          </h2>
+          <div className="flex-1">
+            <TestimonialStats type="total" />
           </div>
         </div>
+        <div className="bg-white dark:bg-[#0F0F12] rounded-xl p-6 flex flex-col border border-gray-200 dark:border-[#1F1F23] ">
+          <h2 className="text-lg font-bold text-gray-900 dark:text-white mb-4 text-left flex items-center gap-2">
+            <Share2 className="w-3.5 h-3.5 text-zinc-900 dark:text-zinc-50" />
+            Form Submissions
+          </h2>
+          <div className="flex-1">
+            <TestimonialStats type="submissions" />
+          </div>
+        </div>
+        <div className="bg-white dark:bg-[#0F0F12] rounded-xl p-6 flex flex-col border border-gray-200 dark:border-[#1F1F23]">
+          <h2 className="text-lg font-bold text-gray-900 dark:text-white mb-4 text-left flex items-center gap-2">
+            <Users className="w-3.5 h-3.5 text-zinc-900 dark:text-zinc-50" />
+            Team Members
+          </h2>
+          <div className="flex-1">
+            <TestimonialStats type="team" />
+          </div>
+        </div>
+      </div>
 
-        <div className="grid grid-cols-1 lg:grid-cols-3 gap-6">
-          <div className="lg:col-span-2 bg-white dark:bg-[#0F0F12] rounded-xl pt-6 flex flex-col  dark:border-[#1F1F23]">
-            <h2 className="text-lg font-bold text-gray-900 dark:text-white mb-4 text-left flex items-center gap-2 border-b pb-3 border-gray-300">
-              <MessageSquare className="w-3.5 h-3.5 text-zinc-900 dark:text-zinc-50 " />
-              Recent Testimonials
-            </h2>
-            <TestimonialList />
-          </div>
-          <div className="bg-white dark:bg-[#0F0F12] rounded-xl p-6 flex flex-col border border-gray-200 dark:border-[#1F1F23]">
-            <h2 className="text-lg font-bold text-gray-900 dark:text-white mb-4 text-left flex items-center gap-2">
-              <Share2 className="w-3.5 h-3.5 text-zinc-900 dark:text-zinc-50" />
-              Share Your Form
-            </h2>
-            <ShareForm />
-          </div>
+      <div className="grid grid-cols-1 lg:grid-cols-3 gap-6">
+        <div className="lg:col-span-2 bg-white dark:bg-[#0F0F12] rounded-xl pt-6 flex flex-col  dark:border-[#1F1F23]">
+          <h2 className="text-lg font-bold text-gray-900 dark:text-white mb-4 text-left flex items-center gap-2 border-b pb-3 border-gray-300">
+            <MessageSquare className="w-3.5 h-3.5 text-zinc-900 dark:text-zinc-50 " />
+            Recent Testimonials
+          </h2>
+          <TestimonialList />
+        </div>
+        <div className="bg-white dark:bg-[#0F0F12] rounded-xl p-6 flex flex-col border border-gray-200 dark:border-[#1F1F23]">
+          <h2 className="text-lg font-bold text-gray-900 dark:text-white mb-4 text-left flex items-center gap-2">
+            <Share2 className="w-3.5 h-3.5 text-zinc-900 dark:text-zinc-50" />
+            Share Your Form
+          </h2>
+          <ShareForm />
         </div>
       </div>
     </div>
--- src/app/(dash)/layout.tsx
+++ src/app/(dash)/layout.tsx
@@ -22,11 +22,15 @@ export default function Layout({ children }: LayoutProps) {
   }
 
   return (
-    <div className={`flex h-screen ${theme === "dark" ? "dark" : ""}`}>
-      <Sidebar />
-      <main className="flex-1 overflow-auto p-6 bg-white  dark:bg-[#0F0F12] pl-10">
-        {children}
-      </main>
-    </div>
+    <html lang="en" className="overflow-hidden">
+      <body className="overflow-x-hidden">
+        <div className={`flex h-screen ${theme === "dark" ? "dark" : ""}`}>
+          <Sidebar />
+          <main className="flex-1 overflow-auto p-6 bg-white  dark:bg-[#0F0F12] pl-10">
+            {children}
+          </main>
+        </div>
+      </body>
+    </html>
   );
 }
